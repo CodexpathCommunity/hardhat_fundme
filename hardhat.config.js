858057d839d72842dda8cadd2c304f69b05fd473
CodexpathCommunity <codexpath3@gmail.com>
2022-12-12 02:19:26 +0100

    chore: fund me acceps contract address on wallet deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,47 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {},
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545",
+            chainId: 31337,
+        },
+    },
+    solidity: "0.8.17",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+        namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+    },
+}
+
